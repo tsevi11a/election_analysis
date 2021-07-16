8ce298e37128f36cb5c93c2d73df2d35f6175122
Trina Sevilla <trina@Trinas-MacBook-Pro.local>
2021-07-16 19:45:06 -0400

    update

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,7 @@ import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
